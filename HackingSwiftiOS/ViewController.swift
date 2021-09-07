23eb3af5528441552efa23ad05ebc4fb52f3e7ed
Prabhu <prabhu8780@gmail.com>
2021-09-07 22:22:15 +0530

    First day

--- HackingSwiftiOS/ViewController.swift
+++ HackingSwiftiOS/ViewController.swift
@@ -12,9 +12,49 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("prabhu")
+
+        multiLine()
+        stringInterPolution()
+        typeOfIntialization()
         // Do any additional setup after loading the view.
     }
 
-
+func multiLine()
+{
+    
+    let multiLineString = """
+    Prabhu
+    nath
+    Reddy
+    """
+    
+    let multiLineString2 = """
+    rsprabhu nath
+    reddy
+"""
+    
+    print(multiLineString)
+    print(multiLineString2)
+    
+}
+    
+ func stringInterPolution()
+ {
+    let isInster = true
+    
+    let stringInterpoltion = "printwithbool\(isInster)"
+    
+    print(stringInterpoltion)
+ }
+    
+    func typeOfIntialization()
+    {
+        let name : String = "Prabhu"
+        let Age : Int = 30
+        let isAvailable : Bool = true
+        let height : Double = 5.9
+        
+        print("MyInfo-->\(name) \(Age) \(isAvailable) \(height)")
+    }
 }
 
